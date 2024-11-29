cb21051bdeff1f8e90dba4344e6585c67ee7b5b3
Timothy <timothyolofson@gmail.com>
2024-11-29 02:12:31 -0800

    6th Advanced E-Portfolio commit.

--- index.js
+++ index.js
@@ -16,6 +16,45 @@
 
 
 
+// const form = document.querySelector("#contact__form");
+
+
+// function contact(event) {
+
+//   form.form.addEventListener("submit", contact);
+
+//   event.preventDefault();
+
+//   const loading = document.querySelector('.modal__overlay--loading');
+//   const success = document.querySelector('.modal__overlay--success');
+
+//   // loading.classList += " modal__overlay--visible";
+//   loading.classList.add(" modal__overlay--visible");
+
+//   emailjs
+//     .sendForm(
+//       'service_5kug93e',
+//       'template_a0rr73f',
+//       event.target,
+//       'NBhrdQIgGtK1W9U0K'
+
+//   ).then(() => {
+
+//     loading.classList.remove("modal__overlay--visible");
+//     success.classList.add("modal__overlay--visible");
+
+//   }).catch(() => {
+
+//     loading.classList.remove("modal__overlay--visible");
+
+//     alert(
+//       "The email service is temporarily unavailable. Please, contact me directly on nuxoxoma@mailgolem.com"
+//     );
+//   })
+// } 
+
+
+
 function contact(event) {
 
   event.preventDefault();
@@ -23,29 +62,35 @@ function contact(event) {
   const loading = document.querySelector('.modal__overlay--loading');
   const success = document.querySelector('.modal__overlay--success');
 
-  loading.classList += " modal__overlay--visible";
+  loading.classList.add("modal__overlay--visible");
 
-  emailjs
-    .sendForm(
-      'service_5kug93e',
-      'template_a0rr73f',
-      event.target,
-      'NBhrdQIgGtK1W9U0K'
+  emailjs.sendForm('service_5kug93e', 'template_a0rr73f', event.target, 'NBhrdQIgGtK1W9U0K'
 
-  ).then(() => {
-
-    loading.classList.remove("modal__overlay--visible");
-    success.classList += " modal__overlay--visible";
+  ).then(() => { 
+  
+    loading.classList.remove('modal__overlay--visible');
+    success.classList.add('modal__overlay--visible');
 
   }).catch(() => {
 
-    loading.classList.remove("modal__overlay--visible");
+    loading.classList.remove('modal__overlay--visible');
 
-    alert(
-      "The email service is temporarily unavailable. Please, contact me directly on nuxoxoma@mailgolem.com"
-    );
-  })
-} 
+    event.preventDefault()
+
+    alert('The Email service is temporarily unavailable , so please contact me diractly on nuxoxoma@mailgolem.com');
+  
+  });
+
+  // setTimeout(() => {
+    
+  //   console.log('Success!');
+
+  // }, 1000);
+
+}
 
 
-// Lesson @ 2:00:00
+function toggleModal() {
+
+  
+}
