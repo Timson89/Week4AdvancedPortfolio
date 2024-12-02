e64764770a39df2586d26c2dee057236b1b73df4
Timothy <timothyolofson@gmail.com>
2024-12-01 23:55:16 -0800

    8th Advanced E-Portfolio commit.

--- index.js
+++ index.js
@@ -2,58 +2,18 @@
 // template_a0rr73f
 // NBhrdQIgGtK1W9U0K
 
+let contrastToggle = false;
 
+function toggleContrast() {
 
-// function sendMail() {
-//   let parms = {
-//     name : document.getElementById("name").value,
-//     email : document.getElementById("email").value,
-//     message : document.getElementById("message").value,
-//   }
+  contrastToggle = !contrastToggle;
 
-//   emailjs.send("service_5kug93e","template_a0rr73f",parms).then(alert('Email Sent!!'))
-// }
-
-
-
-// const form = document.querySelector("#contact__form");
-
-
-// function contact(event) {
-
-//   form.form.addEventListener("submit", contact);
-
-//   event.preventDefault();
-
-//   const loading = document.querySelector('.modal__overlay--loading');
-//   const success = document.querySelector('.modal__overlay--success');
-
-//   // loading.classList += " modal__overlay--visible";
-//   loading.classList.add(" modal__overlay--visible");
-
-//   emailjs
-//     .sendForm(
-//       'service_5kug93e',
-//       'template_a0rr73f',
-//       event.target,
-//       'NBhrdQIgGtK1W9U0K'
-
-//   ).then(() => {
-
-//     loading.classList.remove("modal__overlay--visible");
-//     success.classList.add("modal__overlay--visible");
-
-//   }).catch(() => {
-
-//     loading.classList.remove("modal__overlay--visible");
-
-//     alert(
-//       "The email service is temporarily unavailable. Please, contact me directly on nuxoxoma@mailgolem.com"
-//     );
-//   })
-// } 
-
-
+  if (contrastToggle) {
+    document.body.classList.add('dark-theme');
+  } else {
+    document.body.classList.remove('dark-theme');
+  }
+}
 
 function contact(event) {
 
@@ -79,14 +39,7 @@ function contact(event) {
 
     alert('The Email service is temporarily unavailable , so please contact me diractly on nuxoxoma@mailgolem.com');
   
-  });
-
-  // setTimeout(() => {
-    
-  //   console.log('Success!');
-
-  // }, 1000);
-
+  }); 
 }
 
 let isModalOpen = false;
